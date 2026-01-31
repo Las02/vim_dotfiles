b92e4a79521f33d5169fa7b7f051c979751dd905
las02 <lasse101010@gmail.com>
2026-01-31 18:24:21 +0100

    now send works

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,18 +1,6 @@
 vim.diagnostic.config { virtual_text = false, signs = false, underline = false }
--- keys = {
---   {
---     "<enter>",
---     function()
---       local t = function(keycode)
---         return vim.api.nvim_replace_termcodes(keycode, true, false, true)
---       end
---       vim.api.nvim_feedkeys(t("<Plug>SlimeSendCell"), "n", true)
---     end,
---     desc = "Slime Send Cell",
---   },
--- },
---
-send_slime = function()
+
+local send_slime = function()
   local t = function(keycode)
     return vim.api.nvim_replace_termcodes(keycode, true, false, true)
   end
@@ -20,16 +8,28 @@ send_slime = function()
 end
 
 ENABLE_SLIME = false
+SLIME_PANE_ID = nil
+
 vim.keymap.set('n', '<leader>us', function()
   if ENABLE_SLIME then
+    -- Toggle OFF: restore keymap and close pane
     ENABLE_SLIME = false
     vim.keymap.set('n', '<enter>', '<enter>')
+    if SLIME_PANE_ID then
+      vim.fn.system('tmux kill-pane -t ' .. SLIME_PANE_ID)
+      SLIME_PANE_ID = nil
+    end
+    vim.notify('Slime disabled', vim.log.levels.INFO)
   else
+    -- Toggle ON: create pane to the right and set keymap
     ENABLE_SLIME = true
+    -- Create a new pane to the right and capture its ID
+    local pane_id = vim.fn.system('tmux split-window -h -P -F "#{pane_id}"'):gsub('%s+', '')
+    SLIME_PANE_ID = pane_id
+    -- Configure slime to target the new pane
+    vim.g.slime_default_config = { socket_name = 'default', target_pane = pane_id }
+    vim.b.slime_config = { socket_name = 'default', target_pane = pane_id }
     vim.keymap.set('n', '<enter>', send_slime)
+    vim.notify('Slime enabled (pane: ' .. pane_id .. ')', vim.log.levels.INFO)
   end
 end)
-
--- if ENABLE_SLIME then
---   vim.keymap.set('n', '<enter>', send_slime)
--- end
