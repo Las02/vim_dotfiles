916e61afb1c23767e8ec6f927d6371126035b2dc
las02 <lasse101010@gmail.com>
2026-07-03 08:42:24 +0200

    Remove unuused plugins

--- init.lua
+++ init.lua
@@ -1059,26 +1059,27 @@ vim.lsp.enable 'copilot'
 vim.keymap.set('n', 'gr', vim.lsp.buf.references)
 vim.keymap.set('n', 'gn', vim.lsp.buf.rename)
 
-require('quarto').setup {
-  debug = false,
-  closePreviewOnExit = true,
-  lspFeatures = {
-    enabled = true,
-    chunks = 'curly',
-    languages = { 'r', 'python', 'julia', 'bash', 'html' },
-    diagnostics = {
-      enabled = true,
-      triggers = { 'BufWritePost' },
-    },
-    completion = {
-      enabled = true,
-    },
-  },
-  codeRunner = {
-    enabled = true,
-    default_method = 'slime', -- "molten", "slime", "iron" or <function>
-    ft_runners = {}, -- filetype to runner, ie. `{ python = "molten" }`.
-    -- Takes precedence over `default_method`
-    never_run = { 'yaml' }, -- filetypes which are never sent to a code runner
-  },
-}
+-- quatro stuff
+-- require('quarto').setup {
+--   debug = false,
+--   closePreviewOnExit = true,
+--   lspFeatures = {
+--     enabled = true,
+--     chunks = 'curly',
+--     languages = { 'r', 'python', 'julia', 'bash', 'html' },
+--     diagnostics = {
+--       enabled = true,
+--       triggers = { 'BufWritePost' },
+--     },
+--     completion = {
+--       enabled = true,
+--     },
+--   },
+--   codeRunner = {
+--     enabled = true,
+--     default_method = 'slime', -- "molten", "slime", "iron" or <function>
+--     ft_runners = {}, -- filetype to runner, ie. `{ python = "molten" }`.
+--     -- Takes precedence over `default_method`
+--     never_run = { 'yaml' }, -- filetypes which are never sent to a code runner
+--   },
+-- }
